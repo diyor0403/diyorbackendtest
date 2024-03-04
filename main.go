0c63a31e131f25ce2c97c398069c4a65f5e1d6db
diyor0403 <162126724+diyor0403@users.noreply.github.com>
2024-03-04 18:30:30 +0500

    121 main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Backend")
-	fmt.Println("Hello Backend 2")
+	fmt.Println("Hello Backend 2 murtazo")
 	fmt.Println("tttt")
 	fmt.Println("123456789")
 	fmt.Println("0000000000")
